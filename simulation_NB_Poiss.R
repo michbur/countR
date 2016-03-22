4bf317b11e183c74eadce13a8b7af10431979049
michbur <michalburdukiewicz@gmail.com>
2016-03-22 13:50:04 +0100

    added simulation

---
+++ simulation_NB_Poiss.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(pbapply)
+
+sim_dat <- do.call(rbind, pblapply(10^(-3L:2), function(single_theta)
+  do.call(rbind, lapply(1L:10/2, function(single_lambda) 
+    do.call(rbind, lapply(1L:100, function(single_rep) tryCatch({
+      
+      foci <- lapply(1L:10, function(dummy) rnbinom(600, size = single_theta, mu = single_lambda))
+      names(foci) <- paste0("C", 1L:10)
+      
+      fit_counts(foci, separate = TRUE, model = "all") %>%
+        summary_fitlist %>% 
+        mutate(between = single_theta < upper & single_theta > lower) %>%
+        group_by(model) %>% 
+        summarize(prop = mean(between)) %>%
+        mutate(replicate = single_rep, lambda = single_lambda, theta = single_theta)
+    }, error = function(w) data.frame(model = NA, prop = NA, replicate = NA, lambda = NA, theta = NA))))
+  ))
+))
+
+save(sim_dat, file = "sim_dat.RData")
